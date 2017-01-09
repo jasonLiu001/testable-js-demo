e55ea460c193c357d95f3e805b19e304d0839af4
wangliu <wangliu@isoftstone.com>
2017-01-09 15:55:20 +0800

    init project.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var apiRouter = require('./router/service.js');
 
-app.use('/static', express.static('public'));
+app.use('/public', express.static('public'));
 app.use('/api', apiRouter);
 app.listen(3030);
 console.log('api service started on port:3030 http://localhost:3030');
